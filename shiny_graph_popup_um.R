7ac63369286de68f6030c198d960df505f38bbf8
Carl Higgs <carl.higgs@rmit.edu.au>
2019-03-22 12:24:35 +1100

    updated set up code to properly weight areas when aggregating; also, now produces overall summary csv

--- shiny_graph_popup_um.R
+++ shiny_graph_popup_um.R
@@ -14,13 +14,17 @@ server <- function(input, output) {
   
   # create area_popup
   area_popup <- paste0("<strong>LGA: </strong>", 
-                      um_2016_lga$f2, 
+                      um_2016_lga$lga, 
                       "<br><strong>Public: </strong>", 
-                      um_2016_lga$f5,"%", 
+                      um_2016_lga$peranytree_public,"%", 
                       "<br><strong>Private: </strong>",
-                      um_2016_lga$f4,"%", 
+                      um_2016_lga$peranytree_private,"%", 
                       "<br><strong>Other: </strong>",
-                      um_2016_lga$f3,"%")
+                      um_2016_lga$peranytree_other,"%", 
+                      "<br><strong>Other: </strong>",
+                      um_2016_lga$peranytree_total,"%", 
+                      "<br><strong>Other: </strong>",
+                      um_2016_lga$fullarea_ha," Ha")
   
   # create a reactive value that will store the click position
   data_of_click <- reactiveValues(clickedMarker=NULL)
@@ -29,16 +33,16 @@ server <- function(input, output) {
   output$map <- renderLeaflet({
     leaflet(data = um_2016_lga) %>%
     addProviderTiles("CartoDB.Positron") %>%
-    addPolygons(layerId=~f2, 
-                fillColor = ~pal(f4), 
+    addPolygons(layerId=~lga, 
+                fillColor = ~pal(peranytree_total), 
                 fillOpacity = 0.7, 
                 color = "#BDBDC3", 
                 weight = 1, 
                 popup = area_popup) %>%
     addLegend("bottomleft", 
               pal = pal, 
-              values = ~f4,
-              title = "Public % tree coverage",
+              values = ~peranytree_total,
+              title = "Total % tree coverage",
               opacity = 1)
   })
   # store the click
@@ -47,15 +51,28 @@ server <- function(input, output) {
   })
   
   # Make a barplot or scatterplot depending of the selected point
-  output$plot=renderPlot({
-    area_of_interest=data_of_click$clickedMarker$f2
-    area_stats=as.numeric(um_2016_lga@data[um_2016_lga@data[,'f2']==area_of_interest,c('f5','f4','f3')])
-    area_labels = c(paste0('public/n',area_stats[1]),
-                    paste0('private/n',area_stats[2]),
-                    paste0('other/n',area_stats[3]))
+  output$barplot=renderPlot({
+    area_of_interest=data_of_click$clickedMarker$lga
+    area_stats=as.numeric(um_2016_lga@data[um_2016_lga@data[,'lga']==area_of_interest,c('peranytree_public','peranytree_private','peranytree_other')])
+    area_labels = c(paste0('public (',area_stats[1],'%)'),
+                    paste0('private (',area_stats[2],'%)'),
+                    paste0('other (',area_stats[3],'%)'))
     if(is.null(area_of_interest)){
-        area_of_interest="no area selected"
+        area_of_interest = 'Port Stephens'
+        area_stats = c(5.84,3.07,21.90)
+        area_labels = c("public (5.84%)","private (3.07%)","other (21.9%)"  )
+        pie(x = area_stats,
+            labels=area_labels, 
+            col=c('#7fc97f','#beaed4','#fdc086'),
+            main=area_of_interest,
+            border = '#ffffff')
+        # barplot(as.matrix(area_stats),
+                # horiz=TRUE,
+                # xlim = c(0,100),
+                # col=c('#7fc97f','#beaed4','#fdc086'), 
+                # border='#ffffff')
     }else{
+        
         pie(x = area_stats,
             labels=area_labels, 
             col=c('#7fc97f','#beaed4','#fdc086'),
@@ -69,8 +86,23 @@ server <- function(input, output) {
 ui <- fluidPage(
   br(),
   column(8,leafletOutput("map", height="600px")),
-  column(4,br(),br(),br(),br(),plotOutput("plot", height="300px")),
+  column(4,br(),br(),br(),br(),plotOutput("barplot", height="300px")),
   br()
 )
 
 shinyApp(ui = ui, server = server)
+
+
+
+# Stacked Bar Plot with Colors and Legend
+counts <- table(x = um_2016_lga@data[, c('peranytree_public','peranytree_private','peranytree_other')], row.names = um_2016_lga@data[, f1])
+barplot(counts, main="Car Distribution by Gears and VS",
+  xlab="Number of Gears", col=c("darkblue","red"),
+  legend = rownames(counts))
+
+head(um_2016_lga@data[order(-um_2016_lga@data$peranytree_total),])  
+
+df <- um_2016_lga@data[order(um_2016_lga@data$peranytree_total),]
+left_inches <-  max(strwidth(df[,'lga'], "inch")+0.4, na.rm = TRUE)
+par(mai=c(1.02,left_inches,0.82,0.42))
+barplot(as.matrix(t(df[, c('peranytree_public','peranytree_private','peranytree_other')])),horiz=TRUE,xlim = c(0,100),col=c('#7fc97f','#beaed4','#fdc086'), border='#ffffff', names.arg=df[,'lga'],las=1)
