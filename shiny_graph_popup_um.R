8dd1315de244a624ed506203bbd5ee428a83998f
Carl Higgs <carl.higgs@rmit.edu.au>
2019-03-22 18:06:49 +1100

    updated map stylings and setup.txt

--- shiny_graph_popup_um.R
+++ shiny_graph_popup_um.R
@@ -2,7 +2,7 @@
 # install.packages('leaflet')
 # install.packages('geojsonio')
 
-
+# MAYBE MINICHARTS? https://cran.r-project.org/web/packages/leaflet.minicharts/vignettes/introduction.html
 library(shiny)
 library(leaflet)
 library(geojsonio)
@@ -11,19 +11,27 @@ server <- function(input, output) {
   
   # build data with 2 places
   um_2016_lga <- geojsonio::geojson_read("um_2016_pp_lga.geojson",what = "sp")
+  um_2016_lga@data <- um_2016_lga@data[order(um_2016_lga@data$peranytree_total),]
+  dat = read.csv("um_2016_pp_overall.csv", header = TRUE)
+  left_inches <-  max(strwidth(df[,'lga'], "inch")+0.4, na.rm = TRUE)
+  labels = um_2016_lga@data[,'lga']
+  bar_data <- as.matrix(t(um_2016_lga@data[, c('peranytree_public','peranytree_private','peranytree_other')]))
+  bar_legend_x=ncol(bar_data) + 3    
+  bar_legend_y=max(colSums(bar_data))
   
   # create area_popup
   area_popup <- paste0("<strong>LGA: </strong>", 
                       um_2016_lga$lga, 
-                      "<br><strong>Public: </strong>", 
+                      "<br><strong>Tree area percent</strong>", 
+                      "<br><i>&nbsp&nbspPublic: </i>", 
                       um_2016_lga$peranytree_public,"%", 
-                      "<br><strong>Private: </strong>",
+                      "<br><i>&nbsp&nbspPrivate: </i>", 
                       um_2016_lga$peranytree_private,"%", 
-                      "<br><strong>Other: </strong>",
+                      "<br><i>&nbsp&nbspOther: </i>", 
                       um_2016_lga$peranytree_other,"%", 
-                      "<br><strong>Other: </strong>",
+                     "<br><i>&nbsp&nbspTotal: </i>", 
                       um_2016_lga$peranytree_total,"%", 
-                      "<br><strong>Other: </strong>",
+                      "<br><strong>Data coverage area: </strong>",
                       um_2016_lga$fullarea_ha," Ha")
   
   # create a reactive value that will store the click position
@@ -46,8 +54,9 @@ server <- function(input, output) {
               opacity = 1)
   })
   # store the click
-  observeEvent(input$map_shape_click,{
-    data_of_click$clickedMarker  <- input$map_shape_click
+  observeEvent(input$Map_shape_click,{
+    data_of_click$clickedMarker  <- input$Map_shape_click
+    print(paste0(area_of_interest))
   })
   
   # Make a barplot or scatterplot depending of the selected point
@@ -58,26 +67,40 @@ server <- function(input, output) {
                     paste0('private (',area_stats[2],'%)'),
                     paste0('other (',area_stats[3],'%)'))
     if(is.null(area_of_interest)){
-        area_of_interest = 'Port Stephens'
-        area_stats = c(5.84,3.07,21.90)
-        area_labels = c("public (5.84%)","private (3.07%)","other (21.9%)"  )
-        pie(x = area_stats,
-            labels=area_labels, 
-            col=c('#7fc97f','#beaed4','#fdc086'),
-            main=area_of_interest,
-            border = '#ffffff')
-        # barplot(as.matrix(area_stats),
-                # horiz=TRUE,
-                # xlim = c(0,100),
-                # col=c('#7fc97f','#beaed4','#fdc086'), 
-                # border='#ffffff')
+        par(mai=c(1.02,left_inches,0.82,0.42))
+        # par(xpd=TRUE)
+        barplot(bar_data,
+                horiz=TRUE,
+                xlim = c(0,100),
+                col=c('#7fc97f','#beaed4','#fdc086'), 
+                border='#ffffff', 
+                names.arg=labels,
+                las=1,
+                legend.text=c("Public","Private","Other"),
+                args.legend=list(bty = "n", 
+                                 horiz=TRUE, 
+                                 x=25,
+                                 xjust=0.5,
+                                 yjust=0)
+                )
+                title(paste0("Tree area percent of ",toupper('lga'),"s, by land status."), line=2, adj=0,hjust = -0.25)
     }else{
-        
-        pie(x = area_stats,
-            labels=area_labels, 
-            col=c('#7fc97f','#beaed4','#fdc086'),
-            main=area_of_interest,
-            border = '#ffffff')
+        par(mai=c(1.02,left_inches,0.82,0.42))
+        barplot(bar_data,
+                horiz=TRUE,
+                xlim = c(0,100),
+                col=c('#7fc97f','#beaed4','#fdc086'), 
+                border='#ffffff', 
+                names.arg=replace(as.vector(labels),labels!=area_of_interest,''),
+                las=1,
+                legend.text=c("Public","Private","Other"),
+                args.legend=list(bty = "n", 
+                                 horiz=TRUE, 
+                                 x=25,
+                                 xjust=0.5,
+                                 yjust=0)
+                )
+                title(paste0("Tree area percent of ",toupper('lga'),"s, by land status."), line=2, adj=0,hjust = -0.25)
     }
   })
 }
@@ -86,7 +109,7 @@ server <- function(input, output) {
 ui <- fluidPage(
   br(),
   column(8,leafletOutput("map", height="600px")),
-  column(4,br(),br(),br(),br(),plotOutput("barplot", height="300px")),
+  column(4,plotOutput("barplot", height="600px",width="300px")),
   br()
 )
 
@@ -94,15 +117,29 @@ shinyApp(ui = ui, server = server)
 
 
 
-# Stacked Bar Plot with Colors and Legend
-counts <- table(x = um_2016_lga@data[, c('peranytree_public','peranytree_private','peranytree_other')], row.names = um_2016_lga@data[, f1])
-barplot(counts, main="Car Distribution by Gears and VS",
-  xlab="Number of Gears", col=c("darkblue","red"),
-  legend = rownames(counts))
+# # Pie graph
+        # area_of_interest = 'Port Stephens'
+        # area_stats = c(5.84,3.07,21.90)
+        # area_labels = c("public (5.84%)","private (3.07%)","other (21.9%)"  
+# pie(x = area_stats,
+    # labels=area_labels, 
+    # col=c('#7fc97f','#beaed4','#fdc086'),
+    # main=area_of_interest,
+    # border = '#ffffff')
 
-head(um_2016_lga@data[order(-um_2016_lga@data$peranytree_total),])  
+# # Stacked Bar Plot with Colors and Legend
+# head(um_2016_lga@data[order(-um_2016_lga@data$peranytree_total),])  
 
-df <- um_2016_lga@data[order(um_2016_lga@data$peranytree_total),]
-left_inches <-  max(strwidth(df[,'lga'], "inch")+0.4, na.rm = TRUE)
-par(mai=c(1.02,left_inches,0.82,0.42))
-barplot(as.matrix(t(df[, c('peranytree_public','peranytree_private','peranytree_other')])),horiz=TRUE,xlim = c(0,100),col=c('#7fc97f','#beaed4','#fdc086'), border='#ffffff', names.arg=df[,'lga'],las=1)
+# df <- um_2016_lga@data[order(um_2016_lga@data$peranytree_total),]
+# left_inches <-  max(strwidth(df[,'lga'], "inch")+0.4, na.rm = TRUE)
+# par(mai=c(1.02,left_inches,0.82,0.42))
+# barplot(as.matrix(t(df[, c('peranytree_public','peranytree_private','peranytree_other')])),horiz=TRUE,xlim = c(0,100),col=c('#7fc97f','#beaed4','#fdc086'), border='#ffffff', names.arg=df[,'lga'],las=1)
+# par(mai=c(1.02,left_inches,0.82,0.42))
+# barplot(as.matrix(t(um_2016_lga@data[, c('peranytree_public','peranytree_private','peranytree_other')])),
+                # horiz=TRUE,
+                # xlim = c(0,100),
+                # col=c('#7fc97f','#beaed4','#fdc086'), 
+                # border='#ffffff', 
+                # names.arg=replace(as.vector(test),test!='Cessnock',''),
+                # las=1)
+# text(1,10,'test')
--- um_map.R
+++ um_map.R
@@ -4,27 +4,35 @@ library(leaflet)
 library(magrittr)
 library(knitr)
 
-url_lga = "https://www.dropbox.com/s/eu7lfhc8dexujqb/um_2016_pp_lga.geojson?dl=1"
-file <- "um_2016_pp_lga"
-download.file(url_lga, file)
-um_2016_lga <- rgdal::readOGR(file, "OGRGeoJSON")
-pal <- colorBin(palette = "BuPu", domain = um_2016_lga$f4, bins = 8)
+# url_lga = "https://www.dropbox.com/s/eu7lfhc8dexujqb/um_2016_pp_lga.geojson?dl=1"
+# file <- "um_2016_pp_lga"
+# download.file(url_lga, file)
+# um_2016_lga <- rgdal::readOGR(file, "OGRGeoJSON")
+# pal <- colorBin(palette = "BuPu", domain = um_2016_lga$f4, bins = 8)
 
-# f2 lga
-# percent any tree
-# f3 other
-# fr private
-# f5 public
 
+um_2016_lga <- geojsonio::geojson_read("um_2016_pp_lga.geojson",what = "sp")
+um_2016_lga@data <- um_2016_lga@data[order(um_2016_lga@data$peranytree_total),]
+dat = read.csv("um_2016_pp_overall.csv", header = TRUE)
+left_inches <-  max(strwidth(df[,'lga'], "inch")+0.4, na.rm = TRUE)
+labels = um_2016_lga@data[,'lga']
+bar_data <- as.matrix(t(um_2016_lga@data[, c('peranytree_public','peranytree_private','peranytree_other')]))
+bar_legend_x=ncol(bar_data) + 3    
+bar_legend_y=max(colSums(bar_data))
 # pop values
 area_popup <- paste0("<strong>LGA: </strong>", 
-                      um_2016_lga$f2, 
-                      "<br><strong>Public: </strong>", 
-                      um_2016_lga$f5,"%", 
-                      "<br><strong>Private: </strong>",
-                      um_2016_lga$f4,"%", 
-                      "<br><strong>Other: </strong>",
-                      um_2016_lga$f3,"%")
+                     um_2016_lga$lga, 
+                     "<br><strong>Tree area percent</strong>", 
+                     "<br><i>&nbsp&nbspPublic: </i>", 
+                     um_2016_lga$peranytree_public,"%", 
+                     "<br><i>&nbsp&nbspPrivate: </i>", 
+                     um_2016_lga$peranytree_private,"%", 
+                     "<br><i>&nbsp&nbspOther: </i>", 
+                     um_2016_lga$peranytree_other,"%", 
+                     "<br><i>&nbsp&nbspTotal: </i>", 
+                     um_2016_lga$peranytree_total,"%", 
+                     "<br><strong>Data coverage area: </strong>",
+                     um_2016_lga$fullarea_ha," Ha")
 # plot the map
 leaflet(data = um_2016_lga) %>%
   addProviderTiles("CartoDB.Positron") %>%
